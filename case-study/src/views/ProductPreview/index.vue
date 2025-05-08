6bc80ba7f97d026149021461bb8f35681c5ad08f
Sameer Kamble <sameerkamble1130@gmail.com>
2025-05-08 17:24:55 +0530

    build errors removed-1

--- case-study/src/views/ProductPreview/index.vue
+++ case-study/src/views/ProductPreview/index.vue
@@ -13,7 +13,7 @@
 <div class="flex flex-row mt-8 justify-center">
     <p class="w-1/2 pr-16 leading-relaxed text-gray-500 text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, animi eligendi natus ut neque facere veritatis quod quisquam voluptates laboriosam eius, beatae rerum inventore quos possimus a harum delectus explicabo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident veritatis dolore ducimus harum porro. Alias reiciendis quo assumenda repudiandae molestiae reprehenderit itaque dolorem rem, dolorum sit hic excepturi saepe corrupti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste architecto fuga rerum quae vero earum, dolores eaque ducimus itaque, omnis veniam ratione, placeat dolorum. Cumque rerum quaerat asperiores vero officiis!</p>
     <img
-      src="@/assets/images/productPreview.png"
+      src="@/assets/images/productpreview.png"
       alt="Image 1"
       class="w-1/2 h-[500px] rounded-3xl"
     />
