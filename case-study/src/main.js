3a1c8dce5045f405c78f9d3f10f43c2a584d3272
Sameer Kamble <sameerkamble1130@gmail.com>
2025-05-08 17:15:38 +0530

    build errors removed

--- case-study/src/main.js
+++ case-study/src/main.js
@@ -3,6 +3,11 @@ import './style.css'
 import './assets/main.css'
 import App from './App.vue'
 import 'ant-design-vue/dist/reset.css'
+// main.js or main.ts
+import dayjs from 'dayjs'
+import advancedFormat from 'dayjs/plugin/advancedFormat'
+
+dayjs.extend(advancedFormat)
 
 const app = createApp(App)
 app.mount('#app')
